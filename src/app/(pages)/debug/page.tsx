17364932212e9d8980f300e613d2467752658df2
Pedro Stanzani <pedrohstanzani@gmail.com>
2025-03-02 19:19:18 -0300

    Remove debug page

--- src/app/(pages)/debug/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Input } from "@/components/ui/input";
-
-export default function DebugPage() {
-  return (
-    <main className="px-4">
-      <Input type="email" placeholder="Email" />
-    </main>
-  );
-}
